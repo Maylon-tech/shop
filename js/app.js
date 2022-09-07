bff962d47b1435700c705d691948bbbcb1af21a2
Maylon-tech <minatokiroisen77@gmail.com>
2022-09-07 22:02:13 +0900

    Customizando o COntent Cartgit add .

--- js/app.js
+++ js/app.js
@@ -38,26 +38,98 @@ function ready() {
 
         let input = quantityInputs[i]
         input.addEventListener("change", quantityChanged)
-     }
+    }
+    // Add to cart
+    let addCart = document.getElementsByClassName('add-cart')
+    for (let i = 0;i < addCart.length; i++) {
+
+        let button = addCart[i]
+        button.addEventListener('click', addCartClicked)
+    }
+
+    //Buy Button working.
+    document
+        .getElementsByClassName('btn-buy')
+        .addEventListener('click', buyButtonClicked)
 }
+// Buy Button
+function buyButtonClicked() {
+
+    alert('Your order is Placed!!')
+    let cartContent = document.getElementsByClassName('cart-content')[0]
+    //Enquanto ..
+    while (cartContent.hasChildNodes()){
+        cartContent.removeChild(cartContent.firstChild)
+                
+    }       updateTotal(); void
+    updateTotal()
+}
+
 
 // Quantity change Function
-function quantityChanged() {
+function quantityChanged(event) {
     
     let input = event.target
-    if (NaN(input.value) || input.value <= 0) {
+    if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
+    updateTotal()
 }
 
-
 // Remove Items from cart Function
-function removeCartItem() {
+function removeCartItem(event) {
 
     let buttonClicked = event.target
     buttonClicked.parentElement.remove()
     updateTotal()
 }
+
+// Add To Cart
+function addCartClicked(event) {
+    let button = event.target
+    let shopProducts = button.parentElement
+    let title = shopProducts.getElementsByClassName('product-title')[0].innerText
+    let price = shopProducts.getElementsByClassName('price')[0].innerText
+    let productImg = shopProducts.getElementsByClassName('product-img')[0].src
+    addProductToCart(title, price, productImg)
+    updateTotal()
+}
+
+
+function addProductToCart(title, price, productImg){
+
+    let cartShopBox = document.createElement('div') // Criando elemento DIV para colocar os items selecionados nas Compras
+    cartShopBox.classList.add('cart-box')
+    let cartItems = document.getElementsByClassName('cart-content')[0]
+    let cartItemsNames = cartItems.getElementsByClassName('cart-product-title')
+
+    for (let i = 0;i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == title) {
+            alert("you have already add this item to Cart!")
+            return
+        }
+    }
+
+    let cartBoxContent = `
+        <img src="${productImg}" class="cart-img">
+        <div class="detail-box">
+            <div class="cart-product-title">${title}</div>
+            <div class="cart-price">${price}</div>
+            <input type="number" value="1" class="cart-quantity">
+        </div>
+        <i class='bx bx-trash cart-remove'></i>
+    `
+    cartShopBox.innerHTML = cartBoxContent
+    cartItems.append(cartShopBox)
+    cartShopBox
+        .getElementsByClassName('cart-remove')[0]
+        .addEventListener('click', removeCartItem)
+    cartShopBox
+        .getElementsByClassName('cart-quantity')[0]
+        .addEventListener('change', quantityChanged)
+}
+
+
 
 //Update Total
 function updateTotal() {
@@ -74,7 +146,9 @@ function updateTotal() {
         let price = parseFloat(priceElement.innerText.replace("$", ""))
         let quantity = quantityElement.value
         total = total + (price * quantity)
-
-        document.getElementsByClassName('total-price')[0].innerText = "$", + total
     }
+        // If price contain some Cents Value..
+        total = Math.round(total * 100) / 100
+        document.getElementsByClassName('total-price')[0].innerText = "$" + total
+    
 }
